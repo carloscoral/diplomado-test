e0377e2616fa53bf0c8d8317069cc6831686dac3
Carlos Coral <coralcarlos21@gmail.com>
2023-07-22 09:14:00 -0500

    Update beanstalk

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const port = 8080;
 app.use(cors());
 
 app.get('/', (req, res) => {
-  res.json({ message: 'Hello world!' });
+  res.json({ message: 'Hello from beanstalk!' });
 });
 
 app.listen(port, () => {
